4781ec023b3b390356f88bac772227239ac28417
SuperTiendaGeek <supertiendageek@gmail.com>
2025-03-05 21:41:37 -0500

    Redirigir página principal a /login/

--- mi_proyecto/urls.py
+++ mi_proyecto/urls.py
@@ -17,12 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def home(request):
     return HttpResponse("<h1>¡SuperGeek Reparaciones está en línea!</h1>")
 
+def home_redirect(request):
+    return redirect('/login/')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), 
+    path('', home_redirect),
     path('', include('reparaciones.urls')),  
 ]
